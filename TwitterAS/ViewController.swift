532014e0e73cdbd649cea92849f11ba1e0fb55e0
AdamStehlik <stehlikadam@gmail.com>
2020-06-30 12:12:42 +0200

    Initial Commit

---
+++ TwitterAS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterAS
+//
+//  Created by Adam Stehlik on 30/06/2020.
+//  Copyright © 2020 Adam Stehlik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
